fbc30cdaa4cf890ff37444d84ab72b87b50922e5
katy <katy@katynicholson.uk>
2021-11-14 07:44:18 +0000

    updated

--- inc/page.php
+++ inc/page.php
@@ -3,25 +3,11 @@
 Site Template page builder class
 Katy Nicholson
 https://katystech.blog/
-
-TO DO list:
-
-- Forms: 	Dropdown: If you select the already selected item, the combo dropdown doesn't go away
-		Dropdown and lists: If you set required="required" it requires every element to be selected at once
-		Dropdown and lists: Scroll bar for large lists
-		List: Allow unselect of item in single select list
-		Flyout panel: Scrollbar
-		Form validation - highlight missed/incorrect fields in red
-		Form validation - don't enable button until validation passes (different style for disabled buttons)
-- Page Reload	Prompt for data loss when flyout window is closed
-		Prompt for data loss if browser page is left/reloaded
-
-
-
-
 */
 require_once dirname(__FILE__) . '/auth.php';
 require_once dirname(__FILE__) . '/graph.php';
+require_once dirname(__FILE__) . '/pageBuilder_tables.php';
+require_once dirname(__FILE__) . '/pageBuilder_forms.php';
 define('_NL', "\r\n");
 
 class sitePage {
@@ -32,6 +18,7 @@ class sitePage {
 	var $modAuth;
 	var $modGraph;
 	var $flyout;
+	var $preloadImages = array();
 
 	public $logo;
 
@@ -55,12 +42,14 @@ class sitePage {
 
 			return '<div class="login" tabindex="-1" id="m_login"><div><span>' . $profile->displayName . '</span><span class="light">' . $profile->userPrincipalName . '</span></div>' . $photo . '<ul><li>Role: ' . ($this->modAuth->checkUserRole('Role.User') ? 'User' : '') . ($this->modAuth->checkUserRole('Role.Admin') ? 'Admin' : '') . ($this->modAuth->checkUserRole('Default Access') ? 'Read Only' : '') . '</li><li><a href="https://login.microsoftonline.com/common/wsfederation?wa=wsignout1.0">Sign Out</a></li></ul></div>';
 		}
+		$this->preloadImages[] = '/images/notLoggedIn.png';
 		return '<div class="login" tabindex="-1" id="m_login"><div><span class="loggedout">Not signed in</span></div><span class="userPhoto notLoggedIn"><img src="/images/notLoggedIn.png" /></span><ul><li><a href="' . $_SERVER['REQUEST_URI'] . (strstr($_SERVER['REQUEST_URI'], '?') ? '&' : '?') . 'login=1">Sign in</a></li></ul></div>';
 
 	}
 
 	function initFlyout() {
-		$this->flyout = '<div id="flyout"><div id="flyoutTitle">Loading...</div><div id="flyoutClose" title="Close" onclick="JavaScript:closeFlyout();"> </div><iframe id="flyoutFrame" title="Flyout"></iframe></div>';
+		$this->preloadImages[] = '/images/Close.png';
+		$this->flyout = '<div id="flyout" tabindex="-5"><div id="flyoutTitle">Loading...</div><div id="flyoutClose" title="Close" onclick="JavaScript:closeFlyout();"> </div><div id="flyoutFrame"></div></div>';
 	}
 
 	function printNavigation() {
@@ -101,11 +90,12 @@ class sitePage {
 				if (!$output) {
 					$output = '<div id="navSide">' . _NL;
 				}
-				$output .= '<div class="navSideGroup">' . $navItem->name . _NL;
+				$output .= '<div class="navSideGroup">' . ($navItem->name ? '<span>' . $navItem->name . '</span>' : '') . _NL;
 				if ($navItem->subMenu) {
 					$output .= '<ul>';
 					foreach ($navItem->subMenu as $subItem) {
-						$output .= '<li>' . ($subItem->link ? '<a href="' . $subItem->link . '">'  . $subItem->name . '</a>' : $subItem->name) .  '</li>' . _NL;
+						if ($subItem->icon) $this->preloadImages[] = '/images/sidenav/' . $subItem->icon;
+						$output .= '<li' .  ($subItem->link ? ' onclick="JavaScript:location.href=\'' . $subItem->link . '\';"' : '') . ($subItem->flyoutAction ? ' onclick="JavaScript:openFlyout(\'' . $subItem->flyoutAction . '\', \'' . $subItem->flyoutTitle . '\');"' : '') . ($subItem->icon ? ' style="background-image: url(\'/images/sidenav/' . $subItem->icon . '\');"' : '') . ($subItem->selected ? ' class="selected"' : '') . '>' . ($subItem->link ? '<a href="' . $subItem->link . '">'  . $subItem->name . '</a>' : $subItem->name) .  '</li>' . _NL;
 					}
 					$output .= '</ul>' . _NL;
 				}
@@ -122,20 +112,26 @@ class sitePage {
 	}
 
 	function printFlyoutPage() {
-		return $this->printHead(1) . $this->page . $this->printFoot();
+		return $this->page;
 	}
 
-	function printHead($flyout = '0') {
-		return '<!DOCTYPE html>
+	function printHead() {
+		$output = '<!DOCTYPE html>
 			<html lang="en">
 			<head>
 				<meta name="viewport" content="width=device-width, initial-scale=1">
 				<title>' . $this->title . '</title>
-				<link rel="stylesheet" type="text/css" href="style.css?' . mt_rand(5, 15). mt_rand(5, 15). mt_rand(5, 15). mt_rand(5, 15) . '" />
-				<script type="text/javascript" src="sitetemplate.js?' . mt_rand(5, 15). mt_rand(5, 15). mt_rand(5, 15). mt_rand(5, 15) . '"></script>
+				<link rel="stylesheet" type="text/css" href="style.css?' . mt_rand(5, 15). mt_rand(5, 15). mt_rand(5, 15). mt_rand(5, 15) . '" />';
+
+		foreach ($this->preloadImages as $img) {
+			$output .= '<link rel="preload" as="image" href="' . $img . '">';
+		}
+		$output .= '	<script type="text/javascript" src="sitetemplate.js?' . mt_rand(5, 15). mt_rand(5, 15). mt_rand(5, 15). mt_rand(5, 15) . '"></script>
 				' . $this->script . '
 			</head>
-			<body id="' . ($flyout ? 'flyout' : 'body') . '" tabindex="-5">';
+			<body id="body" tabindex="-5">
+			<div id="ajaxError"><div id="ajaxErrorInner"><span>Something\'s gone wrong!</span>An error ocurred performing that action. Please try refreshing the page.</div></div>';
+		return $output;
 	}
 
 	function printfoot() {
@@ -146,8 +142,8 @@ class sitePage {
 		switch (gettype($content)) {
 			case 'object':
 				switch (get_class($content)) {
-					case 'pageTable': case 'pageForm':
-						$this->page .= $content->output();
+					case 'pageTable': case 'pageForm': case 'infoTip':
+						$this->page .= $content->output($this->preloadImages);
 						break;
 
 					default:
@@ -208,176 +204,20 @@ class navigationItem {
 
 }
 
-class pageTable {
-	private $columns;
-	private $rows;
+class infoTip {
+	public $type;		// info, warning, error
+	private $text;
 
-	function __construct() {
-	}
-
-	function addColumn($column) {
-		$this->columns[] = $column;
-		return $column;
-	}
-
-	function addRow() {
-		$newRow = new pageTableRow($this->columns);
-		$this->rows[] = $newRow;
-		return $newRow;
-	}
-
-	function getColumn($colName) {
-		foreach ($this->columns as $column) {
-			if ($column->name == $colName) return $column;
-		}
-		return new pageTableColumn();
-	}
-
-	function output() {
-		$output = '<div class="table">' . _NL . '<div class="head">' . _NL . '<div class="row">' . _NL;
-		foreach ($this->columns as $column) {
-			$output .= '<div class="cell" ' . ($column->width ? ' style="width: ' . $column->width . 'px; max-width: ' . $column->width . 'px;"' : '') . '>' . $column->text . '</div>' . _NL;
-		}
-		$output .= '</div>' . _NL . '</div>' . _NL . '<div class="body">' . _NL;
-		foreach ($this->rows as $row) {
-			$output .= '<div class="row">' . _NL;
-			foreach ($row->column as $column => $data) {
-				$thisColumn = $this->getColumn($column);
-				$output .= '<div title="' . $data->tooltip . '" class="cell" '. ($thisColumn->width ? ' style="max-width: ' . $thisColumn->width . 'px;"' : '') . '>' . $data->text . '</div>' . _NL;
-			}
-			$output .= '</div>' . _NL;
-		}
-		$output .= '</div>' . _NL . '</div>' . _NL;
-		return $output;
-
-	}
-}
-class pageTableRow {
-	public $column;
-	function __construct($columns) {
-		foreach ($columns as $column) {
-			$this->column[$column->name] = new pageTableCell();
-		}
-	}
-	function output() {
-	}
-}
-
-class pageTableCell {
-	public $text;
-	public $value;
-	public $tooltip;
-}
-
-class pageTableColumn {
-	public $name;
-	public $text;
-	public $width;
-	function __construct($name, $width = '') {
-		$this->name = $name;
-		$this->text = $name;
-		$this->width = $width;
-	}
-}
-
-class pageForm {
-	public $action;
-	public $name;
-	public $fields;
-	function __construct($name, $action) {
-		$this->name = $name;
-		$this->action = $action;
-	}
-
-	function addField($field) {
-		$this->fields[] = $field;
-		return $field;
-	}
-
-	function output() {
-		$output = '<form action="' . $this->action . '" name="' . $this->name . '" method="POST" target="_parent">' . _NL;
-		foreach ($this->fields as $field) {
-			$output .= $field->output();
-		}
-		$output .= '</form>' . _NL;
-		return $output;
-	}
-}
-
-class pageFormField {
-	public $id;
-	public $type;
-	public $name;
-	public $value;
-	public $placeholder;
-	public $label;
-	public $options;
-	public $multiselect;	//list only, 1 = yes
-	public $required;
-	public $height;		//bigtext only
-
-	function __construct($name, $type) {
-		$this->name = $name;
+	function __construct($text, $type='info') {
+		$this->text = $text;
 		$this->type = $type;
-		$this->id = 'f' . uniqid();
 	}
 
-	function output() {
-		$output = '';
-		$required = ($this->required ? '<span class="required" title="This field is required">*</span>' : '');
-		// for dropdown and list, $this->required needs some JS maybe, as just putting "required" in the input means all of them have to be checked/selected.
-		switch ($this->type) {
-			case 'text':
-				$output .= ($this->label ? '<label for="' . $this->id . '">' . $required . $this->label . '</label>' : '') . '<input ' . ($this->required ? 'required ' : '') . 'id="' . $this->id . '" type="text" name="' . $this->name . '" value="' . $this->value . '" placeholder="' . $this->placeholder . '" onchange="JavaScript:setUnsaved();"/>';
-				break;
-			case 'bigtext':
-				$output .= ($this->label ? '<label for="' . $this->id . '">' . $required . $this->label . '</label>' : '') . '<textarea style="height: ' . ($this->height ? $this->height : '90') . 'px;" ' . ($this->required ? 'required ' : '') . 'id="' . $this->id . '" type="text" name="' . $this->name . '"  placeholder="' . $this->placeholder . '" onchange="JavaScript:setUnsaved();"/>' . $this->value . '</textarea>';
-				break;
-			case 'date':
-				$output .= ($this->label ? '<label for="' . $this->id . '">' . $required . $this->label . '</label>' : '') . '<input ' . ($this->required ? 'required ' : '') . 'id="' . $this->id . '" type="date" name="' . $this->name . '" value="' . $this->value . '" onchange="JavaScript:setUnsaved();"/>';
-				break;
-			case 'button':
-				$output .= '<input type="button" name="' . $this->name . '" value="' . $this->value . '" />';
-				break;
-			case 'submit':
-				$output .= '<input type="submit" name="' . $this->name . '" value="' . $this->value . '" />';
-				break;
-			case 'list':
-				// $this->options should be an array of value=>text, e.g. value="item1" text="This is a thing that clicks stuff"
-				if (!$this->label) $this->label = 'List';
-				$outputTable = new pageTable();
-				$headerRow = $outputTable->addColumn(new pageTableColumn($this->label));
-				$headerRow->text = $required . $this->label;
-				foreach ($this->options as $listValue => $listText) {
-					$tableRow = $outputTable->addRow();
-					$thisRowID = 'r' . uniqid();
-					if ($this->multiselect) {
-						$tableRow->column[$this->label]->text = '<input onchange="JavaScript:setUnsaved();" type="checkbox" class="hidden" name="' . $this->name . '" id="' . $thisRowID . '" value="' . $listValue . '" /><label class="list" for="' . $thisRowID . '">' . $listText . '</label>';
-					} else {
-						$tableRow->column[$this->label]->text = '<input onchange="JavaScript:setUnsaved();" type="radio" class="hidden" name="' . $this->name . '" id="' . $thisRowID . '" value="' . $listValue . '" /><label class="list" for="' . $thisRowID . '">' . $listText . '</label>';
-					}
-					$tableRow->column[$this->label]->value = $listValue;
-				}
-				$output .= $outputTable->output();
-				break;
-			case 'dropdown':
-				if (!$this->label) $this->label = 'Dropdown';
-				$output .= '<label for="' . $this->id . '">' . $required . $this->label . '</label>' . '<div class="dropdown" tabindex="-2"><span id="' . $this->id . '" class="placeholder">' . $this->placeholder . '</span><ul>';
-				foreach ($this->options as $listValue => $listText) {
-					$thisRowID = 'r' . uniqid();
-					$thisLabelID = 'l' . uniqid();
-					$output .= '<li><input type="radio" class="hidden" name="' . $this->name . '" id="' . $thisRowID . '" value="' . $listValue . '" onchange="JavaScript:dropdownSelected(\'' . $this->id . '\', \'' . $thisLabelID . '\');"><label id="' . $thisLabelID . '" for="' . $thisRowID . '">' . $listText . '</label></li>';
-				}
-				$output .= '</ul></div>';
-				break;
-			default:
-				$output .= 'Unknown Field type: ' . $this->type;
-				break;
-		}
-		return $output . _NL;
-
+	function output(&$preloadImages) {
+		$img = '/images/Status' . ucfirst($this->type) . '.png';
+		$preloadImages[$img] = $img;
+		return '<div class="infotip' . ($this->type == 'warning' || $this->type == 'error' || $this->type == 'info' ? ' ' . $this->type : '') . '"><span>' . $this->text . '</span></div>' . _NL;
 	}
-
 }
 
 ?>
